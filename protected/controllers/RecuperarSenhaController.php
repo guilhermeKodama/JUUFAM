c0192a371ba714ae47ca3c53a7fae0aa441e1e46
Larissa Ayres <larimayres@gmail.com>
2014-12-16 15:49:56 -0400

    Adicionando tela do esqueci minha senha

---
+++ protected/controllers/RecuperarSenhaController.php
@@ -0,0 +1,47 @@
+<?php
+
+class RecuperarSenhaController extends Controller
+{
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+
+        public function validaEmail($email) {
+             $id = array ($email);
+             $model = Usuario::model()->findAllByPk($id);
+             if (sizeof ($model) > 0){
+                 if ($model[0] ->email == $email){
+                     echo 'Email válido';
+                 }else{
+                     echo 'Email inválido';;
+                 }
+             }                    
+        }
+	// Uncomment the following methods and override them if needed
+	/*
+	public function filters()
+	{
+		// return the filter configuration for this controller, e.g.:
+		return array(
+			'inlineFilterName',
+			array(
+				'class'=>'path.to.FilterClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+
+	public function actions()
+	{
+		// return external action classes, e.g.:
+		return array(
+			'action1'=>'path.to.ActionClass',
+			'action2'=>array(
+				'class'=>'path.to.AnotherActionClass',
+				'propertyName'=>'propertyValue',
+			),
+		);
+	}
+	*/
+}
