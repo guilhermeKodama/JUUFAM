be2ddb1f717219882eef9fea062f6cb2be5f3d1b
Guilherme Kodama <guilherme.kodama@gmail.com>
2015-01-19 20:12:50 -0400

    Implementado cadastro fora do curso

--- juufam.sql
+++ juufam.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: juufam
 -- ------------------------------------------------------
--- Server version	5.6.21
+-- Server version	5.5.40-0ubuntu0.14.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -31,9 +31,10 @@ CREATE TABLE `atleta` (
   `genero` enum('feminino','masculino') NOT NULL,
   `tipo_atleta` enum('egresso','funcionario','ativo') NOT NULL,
   `id_curso` varchar(12) NOT NULL,
+  `status` enum('aprovado','em analise','reprovado') NOT NULL DEFAULT 'aprovado',
   PRIMARY KEY (`cpf`),
   KEY `id_curso_idx` (`id_curso`),
-  CONSTRAINT `id_curso` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `id_curso` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -43,7 +44,7 @@ CREATE TABLE `atleta` (
 
 LOCK TABLES `atleta` WRITE;
 /*!40000 ALTER TABLE `atleta` DISABLE KEYS */;
-INSERT INTO `atleta` VALUES ('21101409','01719550212','19923880','Guilherme Kodama','03/11/1992','masculino','ativo','IE015');
+INSERT INTO `atleta` VALUES ('','12321312314','','Guilherme Egresso','13/13/1312','masculino','egresso','ICC015','em analise'),('','66612313123','','Guilherme Ciencia','12/31/3123','masculino','ativo','ICC015','aprovado'),('','91230130031','','Guilherme Sistema','03/11/1992','masculino','ativo','IE015','aprovado');
 /*!40000 ALTER TABLE `atleta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -64,7 +65,7 @@ CREATE TABLE `chapa` (
   KEY `id_unidade_idx` (`id_unidade`),
   CONSTRAINT `id_evento` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `id_unidade` FOREIGN KEY (`id_unidade`) REFERENCES `unidade` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,8 +74,37 @@ CREATE TABLE `chapa` (
 
 LOCK TABLES `chapa` WRITE;
 /*!40000 ALTER TABLE `chapa` DISABLE KEYS */;
-INSERT INTO `chapa` VALUES (1,'Sistema de Informacao',1,1);
+INSERT INTO `chapa` VALUES (1,'Sistema de Informacao',1,1),(2,'Ciencia da Computacao',3,1);
 /*!40000 ALTER TABLE `chapa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chapa_curso`
+--
+
+DROP TABLE IF EXISTS `chapa_curso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chapa_curso` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id_chapa` int(11) DEFAULT NULL,
+  `id_curso` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_chapa_curso_chapa_idx` (`id_chapa`),
+  KEY `fk_chapa_curso_curso_idx` (`id_curso`),
+  CONSTRAINT `fk_chapa_curso_chapa` FOREIGN KEY (`id_chapa`) REFERENCES `chapa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_chapa_curso_curso` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chapa_curso`
+--
+
+LOCK TABLES `chapa_curso` WRITE;
+/*!40000 ALTER TABLE `chapa_curso` DISABLE KEYS */;
+INSERT INTO `chapa_curso` VALUES (1,1,'IE015'),(2,2,'ICC015');
+/*!40000 ALTER TABLE `chapa_curso` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -123,7 +153,7 @@ CREATE TABLE `evento` (
   `data_end_insc` date NOT NULL,
   `cert_conc_path` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -132,7 +162,7 @@ CREATE TABLE `evento` (
 
 LOCK TABLES `evento` WRITE;
 /*!40000 ALTER TABLE `evento` DISABLE KEYS */;
-INSERT INTO `evento` VALUES (1,'Evento 2015','2014-12-31','2014-12-31','2014-12-31','2014-12-31',NULL);
+INSERT INTO `evento` VALUES (1,'Evento 2015','2014-12-31','2014-12-31','2014-12-31','2014-12-31',NULL),(2,'Evento 2015','2015-01-01','2015-01-01','2015-01-01','2015-01-01',NULL),(3,'Evento 2017','2017-11-03','2017-11-03','2017-11-03','2017-11-03',NULL);
 /*!40000 ALTER TABLE `evento` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -159,6 +189,7 @@ CREATE TABLE `evento_modalidade` (
 
 LOCK TABLES `evento_modalidade` WRITE;
 /*!40000 ALTER TABLE `evento_modalidade` DISABLE KEYS */;
+INSERT INTO `evento_modalidade` VALUES (2,1),(2,2),(2,3),(2,4),(3,1),(3,2),(3,3),(3,4);
 /*!40000 ALTER TABLE `evento_modalidade` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -176,7 +207,7 @@ CREATE TABLE `instituto` (
   PRIMARY KEY (`id`),
   KEY `id_uni_idx` (`id_uni`),
   CONSTRAINT `id_uni` FOREIGN KEY (`id_uni`) REFERENCES `unidade` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -185,7 +216,7 @@ CREATE TABLE `instituto` (
 
 LOCK TABLES `instituto` WRITE;
 /*!40000 ALTER TABLE `instituto` DISABLE KEYS */;
-INSERT INTO `instituto` VALUES (1,'ICOMP',1);
+INSERT INTO `instituto` VALUES (1,'ICOMP',1),(2,'FD',1);
 /*!40000 ALTER TABLE `instituto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -204,7 +235,7 @@ CREATE TABLE `modalidade` (
   `max_inscr` int(11) NOT NULL,
   `genero` enum('masculino','feminino') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -213,10 +244,29 @@ CREATE TABLE `modalidade` (
 
 LOCK TABLES `modalidade` WRITE;
 /*!40000 ALTER TABLE `modalidade` DISABLE KEYS */;
-INSERT INTO `modalidade` VALUES (1,'Futsal','coletivo',5,10,'masculino');
+INSERT INTO `modalidade` VALUES (1,'Futsal','coletivo',5,10,'masculino'),(2,'Natação','individual',3,10,'masculino'),(3,'Volley','coletivo',5,10,'masculino'),(4,'Futsal','coletivo',5,10,'feminino');
 /*!40000 ALTER TABLE `modalidade` ENABLE KEYS */;
 UNLOCK TABLES;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `regulamento`
+--
+
+CREATE TABLE IF NOT EXISTS `regulamento` (
+  `ano` int(4) NOT NULL,
+  `link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `regulamento`
+--
+
+INSERT INTO `regulamento` (`ano`, `link`) VALUES
+(2015, 'http://localhost/juufam/regulamento/regulamento_2015.pdf');
+
+-- --------------------------------------------------------
 --
 -- Table structure for table `repr_atleta`
 --
@@ -255,8 +305,12 @@ CREATE TABLE `time` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_modalidade` int(11) NOT NULL,
   `id_curso` varchar(12) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `id_modalidade_time` (`id_modalidade`),
+  KEY `id_curso_time` (`id_curso`),
+  CONSTRAINT `id_curso_time` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_modalidade_time` FOREIGN KEY (`id_modalidade`) REFERENCES `modalidade` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -265,6 +319,7 @@ CREATE TABLE `time` (
 
 LOCK TABLES `time` WRITE;
 /*!40000 ALTER TABLE `time` DISABLE KEYS */;
+INSERT INTO `time` VALUES (5,1,'IE015');
 /*!40000 ALTER TABLE `time` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -279,9 +334,16 @@ CREATE TABLE `time_atletas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_atleta` varchar(11) NOT NULL,
   `id_time` int(11) NOT NULL,
-  `tipo_atleta` ENUM('atleta','tecnico') NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id_repr` varchar(45) NOT NULL,
+  `status` enum('aprovado','em analise','reprovado') NOT NULL DEFAULT 'aprovado',
+  PRIMARY KEY (`id`),
+  KEY `id_time_time` (`id_time`),
+  KEY `fk_id_atleta_time_idx` (`id_atleta`),
+  KEY `id_repr_time` (`id_repr`),
+  CONSTRAINT `fk_id_atleta_time` FOREIGN KEY (`id_atleta`) REFERENCES `atleta` (`cpf`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_repr_time` FOREIGN KEY (`id_repr`) REFERENCES `usuario` (`login`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `id_time_time` FOREIGN KEY (`id_time`) REFERENCES `time` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -290,6 +352,7 @@ CREATE TABLE `time_atletas` (
 
 LOCK TABLES `time_atletas` WRITE;
 /*!40000 ALTER TABLE `time_atletas` DISABLE KEYS */;
+INSERT INTO `time_atletas` VALUES (6,'91230130031',5,'representante','aprovado'),(8,'66612313123',5,'representante','em analise');
 /*!40000 ALTER TABLE `time_atletas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -304,7 +367,7 @@ CREATE TABLE `unidade` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -313,7 +376,7 @@ CREATE TABLE `unidade` (
 
 LOCK TABLES `unidade` WRITE;
 /*!40000 ALTER TABLE `unidade` DISABLE KEYS */;
-INSERT INTO `unidade` VALUES (1,'Manaus');
+INSERT INTO `unidade` VALUES (1,'Manaus'),(2,'Parintins');
 /*!40000 ALTER TABLE `unidade` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -342,7 +405,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES ('Guilherme Kodama','admin','admin','representante',1);
+INSERT INTO `usuario` VALUES ('Guilherme Kodama','admin','admin','admin',1),('Guilherme Kodama','representante','representante','representante',1),('repr_ciencia','repr_ciencia','123','representante',2);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -355,4 +418,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-12-15 18:06:18
+-- Dump completed on 2015-01-19 20:10:14
--- juufam/protected/controllers/EvaluateExternalRegistrationController.php
+++ juufam/protected/controllers/EvaluateExternalRegistrationController.php
@@ -1,36 +1,59 @@
 <?php
-
-class EvaluateExternalRegistrationController extends Controller
-{
-	public function actionIndex()
-	{
-		$this->render('index');
+class EvaluateExternalRegistrationController extends Controller {
+	
+	public function actionIndex() {
+		$model = new TimeAtletas ( 'search' );
+		$model->unsetAttributes (); // clear any default values
+		if (isset ( $_GET ['TimeAtletas'] ))
+			$model->attributes = $_GET ['TimeAtletas'];
+		
+		$this->render ( 'index', array (
+				'model' => $model 
+		) );
 	}
-
+	
+	public function actionApprove() {
+		$model = $this->loadModel ( $_GET ['id'] );
+		$model->status = "aprovado";
+		$model->update ();
+		
+		$model = new TimeAtletas ( 'search' );
+		$model->unsetAttributes ();
+		$this->render ( 'index', array (
+				'model' => $model 
+		) );
+	}
+	public function actionDecline() {
+		$model = $this->loadModel ( $_GET ['id'] );
+		$model->status = "reprovado";
+		$model->update ();
+		
+		$model = new TimeAtletas ( 'search' );
+		$model->unsetAttributes ();
+		$this->render ( 'index', array (
+				'model' => $model 
+		) );
+	}
+	public function actionBack() {
+		$model = $this->loadModel ( $_GET ['id'] );
+		$model->status = "em analise";
+		$model->update ();
+		
+		$model = new TimeAtletas ( 'search' );
+		$model->unsetAttributes ();
+		$this->render ( 'index', array (
+				'model' => $model 
+		) );
+	}
+	public function loadModel($id) {
+		$model = TimeAtletas::model ()->findByPk ( $id );
+		if ($model === null)
+			throw new CHttpException ( 404, 'The requested page does not exist.' );
+		return $model;
+	}
+	
 	// Uncomment the following methods and override them if needed
 	/*
-	public function filters()
-	{
-		// return the filter configuration for this controller, e.g.:
-		return array(
-			'inlineFilterName',
-			array(
-				'class'=>'path.to.FilterClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-
-	public function actions()
-	{
-		// return external action classes, e.g.:
-		return array(
-			'action1'=>'path.to.ActionClass',
-			'action2'=>array(
-				'class'=>'path.to.AnotherActionClass',
-				'propertyName'=>'propertyValue',
-			),
-		);
-	}
-	*/
+	 * public function filters() { // return the filter configuration for this controller, e.g.: return array( 'inlineFilterName', array( 'class'=>'path.to.FilterClass', 'propertyName'=>'propertyValue', ), ); } public function actions() { // return external action classes, e.g.: return array( 'action1'=>'path.to.ActionClass', 'action2'=>array( 'class'=>'path.to.AnotherActionClass', 'propertyName'=>'propertyValue', ), ); }
+	 */
 }
