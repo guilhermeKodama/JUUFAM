0a310022ba496e0677f35234b049140214e31631
Larissa Ayres <larimayres@gmail.com>
2015-01-20 22:20:18 -0400

    Funcionalidade de CRUD Regulamento (Admin) e Download Regulamento (Representante)

---
+++ juufam/protected/views/regulamento/index.php
@@ -0,0 +1,20 @@
+<?php
+/* @var $this RegulamentoController */
+/* @var $dataProvider CActiveDataProvider */
+
+$this->breadcrumbs=array(
+	'Regulamentos',
+);
+
+$this->menu=array(
+	array('label'=>'Fazer upload de regulamentos', 'url'=>array('create')),
+	array('label'=>'Editar regulamentos existentes', 'url'=>array('admin')),
+);
+?>
+
+<h1>Regulamentos</h1>
+
+<?php $this->widget('zii.widgets.CListView', array(
+	'dataProvider'=>$dataProvider,
+	'itemView'=>'_view',
+)); ?>
